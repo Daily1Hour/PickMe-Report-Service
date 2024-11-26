0941b3db63be2d83dd64720b3ab8ab630646a57a
yxxngjxn <yxxngjxn99@gmail.com>
2024-11-26 17:39:53 +0900

    feat: Report Data Model 작성

---
+++ src/main/java/pickme/report/model/Report.java
@@ -0,0 +1,44 @@
+package pickme.report.model;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+import java.util.List;
+
+@Document(collection = "reports")
+@Data
+@Schema(description = "기업 분석 데이터 모델")
+public class Report {
+
+    @Id
+    private String userId;
+
+    private List<CompanyIndustryReport> reports;
+
+    @Data
+    public static class CompanyIndustryReport {
+        private String companyName;
+        private String industryType;
+        private Date createdAt;
+        private Date updatedAt;
+        private List<CompanyDetail> companyDetails;
+        private List<IndustryDetails> industryDetails;
+    }
+
+    @Data
+    public static class CompanyDetail {
+        private String companyFeatures;
+        private String companyIdealTalent;
+        private String companyNews;
+    }
+
+    @Data
+    public static class IndustryDetails {
+        private String industryFeatures;
+        private String industryNews;
+    }
+
+}
