35f4561437ada2fa1c0bd04c8bf52adc88161b10
yxxngjxn <yxxngjxn99@gmail.com>
2024-12-08 13:52:41 +0900

    fix: Test 무력화

--- src/test/java/pickme/report/ReportApplicationTests.java
+++ src/test/java/pickme/report/ReportApplicationTests.java
@@ -3,7 +3,7 @@ package pickme.report;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class ReportApplicationTests {
 
     @Test
